8b374f6e0384da392030c4cf23bfc643dac27cba
IreneBioque <irenebioque@gmail.com>
2024-07-11 12:40:29 +0200

    fix find by id user use case test

--- __test__/application/uses-cases/find-user-by-id.use-case.spec.ts
+++ __test__/application/uses-cases/find-user-by-id.use-case.spec.ts
@@ -6,41 +6,42 @@ import { UserRepository } from "../../../src/domain/user.repository";
 describe("FindUserByIdUseCase", () => {
   let userRepositoryMock: jest.Mocked<UserRepository>;
   let findUserByIdUseCase: FindUserByIdUseCase;
+  let user: User;
+  let userId: string;
 
   beforeEach(() => {
+    userId = faker.string.uuid();
+    user = new User(userId, faker.person.firstName(), faker.internet.email());
+
     userRepositoryMock = {
       save: jest.fn(),
-      findById: jest.fn(),
+      findById: jest.fn().mockResolvedValue(user),
     };
 
     findUserByIdUseCase = new FindUserByIdUseCase(userRepositoryMock);
   });
 
-  it("should return a user when it exists", async () => {
-    const userId = faker.datatype.uuid();
-    const expectedUser = new User(
-      userId,
-      faker.person.firstName(),
-      faker.internet.email()
-    );
-
-    userRepositoryMock.findById.mockResolvedValueOnce(expectedUser);
-
-    const result = await findUserByIdUseCase.execute(userId);
-
-    expect(result).toEqual(expectedUser);
-    expect(userRepositoryMock.findById).toHaveBeenCalledTimes(1);
-    expect(userRepositoryMock.findById).toHaveBeenCalledWith(userId);
+  describe("when the user exists", () => {
+    it("should return the entity", async () => {
+      const result = await findUserByIdUseCase.execute(userId);
+      expect(result).toEqual(user);
+    });
   });
 
-  it("should throw an error when the user does not exist", async () => {
-    const userId = faker.string.uuid();
+  describe("when the user does not exist", () => {
+    beforeEach(() => {
+      userRepositoryMock.findById.mockResolvedValueOnce(null);
+    });
 
-    userRepositoryMock.findById.mockResolvedValueOnce(null);
+    it("should throw an error", async () => {
+      await expect(findUserByIdUseCase.execute(userId)).rejects.toThrow(
+        "User not found"
+      );
+    });
+  });
 
-    await expect(findUserByIdUseCase.execute(userId)).rejects.toThrow(
-      "User not found"
-    );
+  it("should call findById function", async () => {
+    await findUserByIdUseCase.execute(userId);
     expect(userRepositoryMock.findById).toHaveBeenCalledTimes(1);
     expect(userRepositoryMock.findById).toHaveBeenCalledWith(userId);
   });
